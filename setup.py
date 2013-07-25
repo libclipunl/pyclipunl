8c753c06eaae3e63d06182f102685c2ab624a54f
David <david.nonamedguy@gmail.com>
2013-07-25 18:40:54 +0100

    Added some more stuff to the setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,13 @@ setup(
         name='pyclipunl',
         version='0.0.1',
         description='Library to scrape UNL\'s CLIP (https://clip.unl.pt) data',
+        long_description="""This library allows people with credentials
+to the UNL (Universidade Nova de Lisboa) CLIP system, to
+programatically access their data""",
         author='David Serrano',
         author_email='david.nonamedguy@gmail.com',
         url="https://github.com/libclipunl/pyclipunl",
         py_modules=['ClipUNL'],
-        requires=['BeautifulSoup==3'],
+        requires=['BeautifulSoup (==3.0)'],
         license='MIT'
     )
